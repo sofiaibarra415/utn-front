2aaeddc2f2cdc9f942eceb2fa8c8ce51e19aeeb6
Ezequiel Agustin Ibarra <82052522+ezeagusibarra001@users.noreply.github.com>
2023-08-11 19:05:10 -0300

    fix

--- src/components/novedades/novedadItem.js
+++ src/components/novedades/novedadItem.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const NovedadItem = (props) => {
-    const {nombre, testimonio, imagen, body} = props;
-    const imagenData = `https://res.cloudinary.com/dkpc18twj/image/upload/c_fill,h_100,w_100/${imagen}`
-    return(
-        <div className="novedades">
-            <h1 className='first'>{nombre}</h1>
-            <h2 className='testimonio'>{testimonio}</h2>
-            <img className='foto' alt='prop'  src={imagenData}/>
-            <div dangerouslySetInnerHTML={{ __html: body}}/>
-            <hr/>
-        </div>
-    );
-}
+  const { nombre, testimonio, imagen } = props;
+  const imagenData = imagen
+    ? `https://res.cloudinary.com/dkpc18twj/image/upload/c_fill,h_100,w_100/${imagen}`
+    : null;
+  return (
+    <div className="novedades">
+      <h1 className="first">{nombre}</h1>
+      <h2 className="testimonio">{testimonio}</h2>
+      {imagenData && <img className="foto" alt="prop" src={imagenData} />}
+      <hr />
+    </div>
+  );
+};
 export default NovedadItem;
--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,35 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import NovedadItem from "../components/novedades/novedadItem";
 
 const NovedadesPage = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [novedades, setNovedades] = useState([]);
 
-    const [loading, setLoading] = useState(false);
-    const [novedades, setNovedades] = useState([]);
+  useEffect(() => {
+    const cargarNovedades = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get("http://localhost:3000/api/novedades");
+        console.log(response.data);
+        setNovedades(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    cargarNovedades();
+  }, []);
 
-    useEffect(() => {
-        const cargarNovedades = async () => {
-            setLoading(true);
-            const response = await axios.get('http://localhost:3000/api/novedades');
-            console.log(response.data)
-            setNovedades(response.data);
-            setLoading(false);
-        };
-        cargarNovedades();
-    }, []);
-    
-    return (
-        <section className="holder">
-            <h2 className='primero'>Contanos tu experiencia siguiendo los planes de #SofiaNutrición y comparti tu foto de alguna receta que hayas hecho!</h2>
-            {loading ? (
-                <p>Cargando...</p>
-            ) : (
-                novedades.map(item => <NovedadItem key={item.id}
-                    nombre={item.nombre} testimonio={item.testimonio}
-                    imagen={item.img_id} body={item.cuerpo} />)
-
-            )}
-        </section>
-    );
-}
+  return (
+    <section className="holder">
+      <h2 className="primero">
+        Contanos tu experiencia siguiendo los planes de #SofiaNutrición y
+        comparti tu foto de alguna receta que hayas hecho!
+      </h2>
+      {loading ? (
+        <p>Cargando...</p>
+      ) : (
+        novedades.map((item) => (
+          <NovedadItem
+            key={item.id}
+            nombre={item.nombre}
+            testimonio={item.testimonio}
+            imagen={item.img_id}
+          />
+        ))
+      )}
+    </section>
+  );
+};
 export default NovedadesPage;
