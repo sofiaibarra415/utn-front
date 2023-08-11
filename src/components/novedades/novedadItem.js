3e7c096f6782aa7814f939b9a77e01bfb1a042f5
sofiaibarra415 <127771748+sofiaibarra415@users.noreply.github.com>
2023-08-10 22:52:48 -0300

    clase 10/08

---
+++ src/components/novedades/novedadItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NovedadItem = (props) => {
+    const {nombre, testimonio, imagen, body} = props;
+    const imagenData = `https://res.cloudinary.com/dkpc18twj/image/upload/c_fill,h_100,w_100/${imagen}`
+    return(
+        <div className="novedades">
+            <h1 className='nombres'>{nombre}</h1>
+            <h2 className='testimonio'>{testimonio}</h2>
+            <img className='foto' alt='prop'  src={imagenData}/>
+            <div dangerouslySetInnerHTML={{ __html: body}}/>
+            <hr/>
+        </div>
+    );
+}
+export default NovedadItem;
--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NovedadItem from "../components/novedades/novedadItem";
+
 const NovedadesPage = (props) => {
+
+    const [loading, setLoading] = useState(false);
+    const [novedades, setNovedades] = useState([]);
+
+    useEffect(() => {
+        const cargarNovedades = async () => {
+            setLoading(true);
+            const response = await axios.get('http://localhost:3000/api/novedades');
+            console.log(response.data)
+            setNovedades(response.data);
+            setLoading(false);
+        };
+        cargarNovedades();
+    }, []);
+    
     return (
-<main class="holder">
-        <h2>Novedades</h2>
-        <div class="novedades">
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem quos, libero ea accusamus aliquam facilis soluta corporis temporibus nobis quasi dolorum, eveniet rem similique id rerum amet repellat dolores.</p>
-            <hr/>
-        </div>
-        <div class="novedades">
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem quos, libero ea accusamus aliquam facilis soluta corporis temporibus nobis quasi dolorum, eveniet rem similique id rerum amet repellat dolores.</p>
-            <hr/>
-        </div>
-        <div class="novedades">
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem quos, libero ea accusamus aliquam facilis soluta corporis temporibus nobis quasi dolorum, eveniet rem similique id rerum amet repellat dolores.</p>
-            <hr/>
-        </div>
-    </main>
+        <section className="holder">
+            <h2 className='primero'>Contanos tus experiencias siguiendo los planes de #SofiaNutrición y comparti tu foto de alguna receta que hayas hecho!</h2>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                novedades.map(item => <NovedadItem key={item.id}
+                    nombre={item.nombre} testimonio={item.testimonio}
+                    imagen={item.img_id} body={item.cuerpo} />)
+
+            )}
+        </section>
     );
 }
 export default NovedadesPage;
