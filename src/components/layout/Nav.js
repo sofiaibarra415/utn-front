8b4b4829d676a738f2a4b7f38e21662e2e6140a7
sofiaibarra415 <127771748+sofiaibarra415@users.noreply.github.com>
2023-08-16 21:05:49 -0300

    clase 15/08

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import '../../style/nav.css';
-const Nav = (props) => {
+const Nav = () => {
+    const location = useLocation();
     return(
         <nav>
         <ul class="">
-            <li><Link className="activo" to="/">Inicio</Link></li>
-            <li><Link to="/recetas">Recetas</Link></li>
-            <li><Link to="/sugerencias">Sugerencias</Link></li>
-            <li><Link to="/Tips">Tips</Link></li>
-            <li><Link to="/novedades">Novedades</Link></li>
-            <li><Link to="/contacto">Contacto</Link></li>
+            <li><Link className={location.pathname === "/" && "activo"} to="/">Inicio</Link></li>
+            <li><Link className={location.pathname === "/recetas" && "activo"} to="/recetas">Recetas</Link></li>
+            <li><Link className={location.pathname === "/sugerencias" && "activo"} to="/sugerencias">Sugerencias</Link></li>
+            <li><Link className={location.pathname === "/Tips" && "activo"} to="/Tips">Tips</Link></li>
+            <li><Link className={location.pathname === "/novedades" && "activo"} to="/novedades">Novedades</Link></li>
+            <li><Link className={location.pathname === "/contacto" && "activo"} to="/contacto">Contacto</Link></li>
         </ul>
     </nav>
     );
--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -1,31 +1,64 @@
 import React from "react";
 import '../style/pages/contacto.css'
+import {useState} from 'react';
+import axios from 'axios';
 const ContactoPage = (props) => {
+
+const initialForm = {
+    nombre:'',
+    email:'',
+    telefono:'',
+    mensaje:''
+}
+const [sending, setSending] = useState(false);
+const [msg, setMsg] = useState('');
+const [formData, setFormData] = useState(initialForm);
+
+const handleChange = e => {
+    const {name, value} = e.target;
+    setFormData(oldData => ({
+        ...oldData,
+        [name]: value
+    }));
+}
+const handleSubmit = async e =>{
+    e.preventDefault();
+    setMsg('');
+    setSending(true)
+    const response = await axios.post('http://localhost:3000/api/contacto', formData);
+    setSending(false);
+    setMsg(response.data.message);
+    if(response.data.error ===false){
+        setFormData(initialForm)
+    }
+}
     return (
 <main class="holder contacto">
         <div>
             <h2 className="contacto">Contacto</h2>
-            <form class="formulario">
+            <form action="/contacto"  method="post" className="formulario" onSubmit={handleSubmit}>
                 <p className="correcto">
                     <label for="nombre">Nombre: </label>
-                    <input type="text"/>
+                    <input className="espacio" type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
                 </p>
                 <p className="correcto">
                     <label for="email">Email: </label>
-                    <input type="text"/>
+                    <input className="espacio" type="text"name="email" value={formData.email} onChange={handleChange}/>
                 </p>
                 <p className="correcto">
                     <label for="telefono">Telefono: </label>
-                    <input type="text"/>
+                    <input className="espacio" type="text" name="telefono" value={formData.telefono} onChange={handleChange}/>
                 </p>
                 <p className="correcto">
-                    <label for="mensaje">Mensaje: </label>
-                    <textarea name=""></textarea>
+                    <label for="mensaje" >Mensaje: </label>
+                    <textarea className="espacio" name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                 </p>
                 <p className="correcto">
                     <input type="submit" value="Enviar"/>
                 </p>
             </form>
+            {sending ? <p>Enviando...</p>: null}
+            {msg ? <p>{msg}</p> : null}
         </div>
         <div class="datos">
             <h2 className="contacto">Otras vias de comunicacion</h2>
